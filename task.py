9f9aaf1c8364723ca1eecb06a56d950b68b8d609
KhaledHaAlmsoud <Khaled.Almsoud.rae.js@tuwaiq.edu.sa>
2022-02-20 15:01:17 +0300

    intro python

---
+++ task.py
@@ -0,0 +1,26 @@
+help ("lambda") 
+help ("def")
+help ("in")
+help("yield")
+
+
+myName = myFreind = MyLegend = "Khaled"
+
+
+my_name , my_age , my_gender = "khaled" , "24" , "male"
+
+
+
+x = 10 
+y = 2.5
+
+
+z = x+y
+
+
+x = str (y)
+
+Name = "Khaled Almsoud" 
+
+
+
