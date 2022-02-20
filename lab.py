728c00e0be1a76659dd5e88ae634183d9604a5b0
OthmanAlrasheedi <othman.mousa.rae.js@tuwaiq.edu.sa>
2022-02-20 14:59:54 +0300

    new lab

---
+++ lab.py
@@ -0,0 +1,20 @@
+
+help("lambda")
+help("def")
+help("in")
+help("yield")
+
+
+othman = khalid = aqeil = "اشخاص جيدين"
+
+name, age = "Othman", "24"
+
+x = 71
+y = 7.1
+z = x+y
+print(type(z))
+
+converToString = str(x)
+
+
+name = "Othman Alrashdeei"
