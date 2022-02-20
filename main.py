ed8f36ad8a06c06437761a55d23ecfcf3db1dc8a
jamelhfarhan <jamelh.farhan.rae.js@tuwaiq.edu.sa>
2022-02-20 14:49:36 +0300

    new solve

---
+++ main.py
@@ -0,0 +1,29 @@
+#Use the suitable function to search about lambda, def, in, and yield keywords.
+help("lambda")
+help("def")
+help("in")
+help("yield")
+# Assign One Value to Multiple Variables.
+my_name = myNameFrsind = mYSecend_name= "jamelh"
+
+
+#- Assign Many Values to Multiple Variables
+age , myfivertfood= "apple" ,25
+
+
+# Define “x“ variable has an integer value and “y” has a float value.
+x=2 
+y= 3.4
+
+
+# Apply implicit type conversion in new variable “z”.
+
+z= 22
+print (z)
+# Apply explicit type conversion on the x variable.
+
+x=44
+print(str (x))
+ # Define “name” variable has a value equals “Your Name”.
+
+Your_Name="jamelah "
