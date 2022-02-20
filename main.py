9ace9a14b68031ca89a4b9fefdbe54620e300f73
Mohammmed1101 <95057058+Mood112@users.noreply.github.com>
2022-02-20 15:17:22 +0300

    fixed

---
+++ main.py
@@ -0,0 +1,36 @@
+# Use the suitable function to search about lambda, def, in, and yield keywords.
+# help("lambda")
+# help("def")
+# help("in")
+# help("yield")
+
+
+# Assign One Value to Multiple Variables
+variable1 = variable2 = variable3 = 4
+print
+print(variable1)
+print(variable2)
+print(variable3)
+
+# Assign Many Values to Multiple Variables.
+variableOne , variableTwo = "Hi" , "Hello"
+print(variableOne)
+print(variableTwo)
+
+# Define “x“ variable has an integer value and “y” has a float value.
+x = 2
+y = 2.0
+
+
+# Apply implicit(ضمني) type conversion in new variable “z”.
+z = x + y
+
+# Apply explicit type conversion on the x variable.
+exVar = int(y)
+
+
+
+# Define “name” variable has a value equals “Your Name”.
+name = "Mohammed Alqarni"
+print(name)
+
